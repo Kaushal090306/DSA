835a8d80c3d9deb5bc103f74c05381679c264a12
kaushal savaliya <166236335+Kaushal090306@users.noreply.github.com>
2025-07-29 23:45:50 +0530

    Update Maximum Product Subarray.cpp

--- Array/Maximum Product Subarray.cpp
+++ Array/Maximum Product Subarray.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int p=INT_MIN,c=1;
+        for(int i=0;i<nums.size();i++){
+            c=c*nums[i];
+            p=max(p,c);
+            if(c==0){
+                c=1;
+            }
 
+        }
+        c=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            c=c*nums[i];
+            p=max(p,c);
+            if(c==0){
+                c=1;
+            }
+
+        }
+
+        return p;
+    }
+};

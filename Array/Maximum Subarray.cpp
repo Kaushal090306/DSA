0cf82bffb67330839a5315eb4007f2cdeefa64aa
kaushal savaliya <166236335+Kaushal090306@users.noreply.github.com>
2025-07-29 23:44:33 +0530

    Update Maximum Subarray.cpp

--- Array/Maximum Subarray.cpp
+++ Array/Maximum Subarray.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0,count=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+                sum = sum+nums[i];
+                count=max(count,sum);
+                if(sum<=0){
+                    sum=0;
+                }
+        }
+        return count;
+    }
+};

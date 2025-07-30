0d29f0b91364eadd482faa3f975a3ad47b7d01c1
kaushal savaliya <166236335+Kaushal090306@users.noreply.github.com>
2025-07-30 21:41:03 +0530

    Update Longest Subarray with Maximum Bitwise And.cpp

--- Daily Questions/Longest Subarray with Maximum Bitwise And.cpp
+++ Daily Questions/Longest Subarray with Maximum Bitwise And.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int m = *max_element(nums.begin(), nums.end());
+        int s=0, ms=0;
+        cout << m;
+        for (int num : nums){
+            if(num == m){
+                s++;
+                if(s > ms){
+                    ms = s;
+                }
+            }else{
+                s=0;
+            }
+        }
+        return ms;
+}
+};

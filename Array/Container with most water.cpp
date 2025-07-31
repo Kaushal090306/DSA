0958b12dc50de1babda4c72ac65c7638f1e14de6
kaushal savaliya <166236335+Kaushal090306@users.noreply.github.com>
2025-07-31 23:28:03 +0530

    Update Container with most water.cpp

--- Array/Container with most water.cpp
+++ Array/Container with most water.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0,r=height.size()-1,ans=0;
+
+        while(l<r){
+            int x=min(height[l],height[r])*(r-l);
+            ans=max(ans,x);
+
+            if(height[l]<height[r]) l++;
+            else r--;
+        }
+        return ans;
+    }
+};

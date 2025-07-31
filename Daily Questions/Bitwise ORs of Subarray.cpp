e40bd56e171101e02b24e8c9887315567074456b
kaushal savaliya <166236335+Kaushal090306@users.noreply.github.com>
2025-07-31 22:30:22 +0530

    Update Bitwise ORs of Subarray.cpp

--- Daily Questions/Bitwise ORs of Subarray.cpp
+++ Daily Questions/Bitwise ORs of Subarray.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& arr) {
+        set<int> curr;
+        set<int> ans;
 
+        for(int i:arr){
+            set<int> next;
+
+            next.insert(i);
+
+            for(int j:curr){
+                next.insert(i|j);
+            }
+            ans.insert(next.begin(),next.end());
+            curr=next;
+        }
+        return ans.size();
+
+    }
+};

11547db6a6889c315ce636ea54a32d42dbc57f26
kaushal savaliya <166236335+Kaushal090306@users.noreply.github.com>
2025-08-02 20:55:48 +0530

    Update Counting Bits.cpp

--- Binary/Counting Bits.cpp
+++ Binary/Counting Bits.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1,0);
+        int x=1;
 
+        for(int i=1;i<=n;i++){
+            if(x*2==i){
+                x=i;
+            }
+            ans[i]=ans[i-x]+1;
+        }
+        return ans;
+
+    }
+};

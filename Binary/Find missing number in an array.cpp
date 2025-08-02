4a989bc4556cdacecf50892fcbecd6a81a411b0d
kaushal savaliya <166236335+Kaushal090306@users.noreply.github.com>
2025-08-02 21:01:32 +0530

    Update Find missing number in an array.cpp

--- Binary/Find missing number in an array.cpp
+++ Binary/Find missing number in an array.cpp
@@ -1,1 +1,16 @@
+//Problem : https://leetcode.com/problems/missing-number/ 
 
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=n*(n+1)/2;
+        int sum2=0;
+        for(int i=0;i<n;i++){
+            sum2=sum2+nums[i];
+        }
+        int x=sum-sum2;
+
+        return x;
+    }
+};

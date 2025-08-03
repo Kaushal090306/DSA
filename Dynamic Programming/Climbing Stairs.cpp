23ec3d200c651a9166fd4796baeaf09c100a1684
kaushal savaliya <166236335+Kaushal090306@users.noreply.github.com>
2025-08-03 21:57:43 +0530

    Update Climbing Stairs.cpp

--- Dynamic Programming/Climbing Stairs.cpp
+++ Dynamic Programming/Climbing Stairs.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0||n==1||n==2){
+            return n;
+        }
+        int curr=2,prev=1,sum=0,temp=0;
+        for(int i=3;i<=n;i++){
+            sum=curr+prev;
+            temp=curr;
+            curr=curr+prev;
+            prev=temp;
 
+        }
+        return sum;
+    }
+};

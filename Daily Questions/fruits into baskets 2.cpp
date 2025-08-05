8c11f6d86919ddf80398f51bb0781e5ad16c64b5
kaushal savaliya <166236335+Kaushal090306@users.noreply.github.com>
2025-08-05 22:00:06 +0530

    Update fruits into baskets 2.cpp

--- Daily Questions/fruits into baskets 2.cpp
+++ Daily Questions/fruits into baskets 2.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int c=0;
+        for(int i=0;i<fruits.size();i++){
+            for(int j=0;j<fruits.size();j++){
+                if(fruits[i]<=baskets[j]){
+                    c++;
+                    baskets[j]=0;
+                    break;
+                }
+            }
+        }
+        return fruits.size()-c;
+    }
+};
